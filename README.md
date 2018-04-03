a172de8427810987b08b75ae6b475922eaee0b65
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2018-04-03 20:38:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Readrr
+A speed reading thing I built for myself. You can use it too if you want.
+
+[![forthebadge](https://forthebadge.com/images/badges/powered-by-electricity.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/gluten-free.svg)](https://forthebadge.com)
+
+## Instructions
+Just add the text you want to read and then press the Start button. It will display each word in text you added.
+
+## Rationale
+When you focus on the words and something is quickly displaying those words for you, you realize you can actually read pretty fast and can cover a lot of text much faster than you usually would.
---
+++ main.js
@@ -0,0 +1,21 @@
+
+const stage1s = Array.from(document.querySelectorAll('.stage-1'))
+
+document.querySelector('.main-container__start').addEventListener('click', () => {
+    const content = document.querySelector('.main-container__area').value
+    stage1s.forEach(stage1 => stage1.classList.add('gone'))
+    document.querySelector('.stage-2').classList.remove('gone')
+    displayWord(content.split(' '), 0)
+})
+
+function displayWord(wordlist, index) {
+    setTimeout(() => {
+        if (index < wordlist.length) {
+            document.querySelector('.main-container__display').innerHTML = wordlist[index]
+            displayWord(wordlist, index+1)
+        } else {
+            stage1s.forEach(stage1 => stage1.classList.remove('gone'))
+            document.querySelector('.stage-2').classList.add('gone')
+        }
+    }, 200)
+}
